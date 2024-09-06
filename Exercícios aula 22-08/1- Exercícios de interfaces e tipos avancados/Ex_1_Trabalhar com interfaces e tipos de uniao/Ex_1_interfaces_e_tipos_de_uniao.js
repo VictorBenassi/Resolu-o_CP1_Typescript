aefbf00e48cbb9b77b7a6e5e5723a163fdf98cd1
VictorBenassi <victorbenassi0@gmail.com>
2024-09-06 11:38:49 -0300

    Commit

--- Exercícios aula 22-08/1- Exercícios de interfaces e tipos avancados/Ex_1_Trabalhar com interfaces e tipos de uniao/Ex_1_interfaces_e_tipos_de_uniao.js
+++
@@ -1,8 +0,0 @@
-// Função que recebe um Produto e uma FormaPagamento
-function exibirProdutoComPagamento(produto, pagamento) {
-    return "Produto: ".concat(produto.nome, ", Categoria: ").concat(produto.categoria, ", Pre\u00E7o: R$").concat(produto.preco.toFixed(2), ", Forma de Pagamento: ").concat(pagamento, ".");
-}
-// Exemplo de uso
-var produtoExemplo = { nome: 'Ventilador', preco: 150, categoria: 'Eletrodomésticos' };
-var formaPagamentoExemplo = 'pix';
-console.log(exibirProdutoComPagamento(produtoExemplo, formaPagamentoExemplo));
--- Exercícios aula 22-08/3 - Exercícios de módulos e namespaces/Ex_1_ Organizar o codigo usando modulos/Principal1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Importação da classe Pedido
-var Pedido_1 = require("./Pedido");
-var Cliente_1 = require("./Cliente");
-// Criação de instâncias de Pedido e Cliente
-var clienteExemplo = new Cliente_1.Cliente('Victor', 'victor@ig.com.br');
-var pedidoExemplo = new Pedido_1.Pedido(clienteExemplo, 'Smartphone', 1800);
-console.log(pedidoExemplo.exibirPedido());
